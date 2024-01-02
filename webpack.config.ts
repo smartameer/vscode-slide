0a58ee6a40a17e2d9fabd5c32140c7d5eff7e8bc
Ameer <smartameer@icloud.com>
2024-01-02 11:08:16 +0530

    Adding config

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,12 +2,10 @@ import * as path from 'path'
 import * as webpack from 'webpack'
 
 const extensionConfig: webpack.Configuration = {
-  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  target: 'node',
+	mode: 'none',
+  entry: './src/extension.ts',
   output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'build'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2'
@@ -33,7 +31,7 @@ const extensionConfig: webpack.Configuration = {
   },
   devtool: 'nosources-source-map',
   infrastructureLogging: {
-    level: "log", // enables logging required for problem matchers
+    level: "log",
   },
 }
 
@@ -56,5 +54,5 @@ const webviewConfig: webpack.Configuration = {
       },
     ],
   },
-};
+}
 module.exports = [ extensionConfig, webviewConfig ]
